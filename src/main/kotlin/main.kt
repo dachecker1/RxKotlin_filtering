a8880e502596827212a5e5ccbbe382454c9accab
andrenivisnk <andrenivinsk@gmail.com>
2022-10-03 09:28:47 +0300

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,95 @@
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.subjects.PublishSubject
+
+fun main(args: Array<String>) {
+
+    exampleOf("IgnoreElements") {
+        val subscriptions = CompositeDisposable()
+
+        val cannedProjects = PublishSubject.create<String>()
+
+        subscriptions.add(cannedProjects
+            .ignoreElements()
+            .subscribeBy {
+                println("Complete")
+            }
+        )
+
+        cannedProjects.onNext(mayTheOdds)
+        cannedProjects.onNext(liveLongAndProsper)
+        cannedProjects.onNext(mayTheForce)
+
+        cannedProjects.onComplete()
+    }
+
+    exampleOf("IgnoreElementAt") {
+        val subscriptions = CompositeDisposable()
+
+        val quotes = PublishSubject.create<String>()
+
+        subscriptions.add(
+            quotes
+                .elementAt(2)
+                .subscribeBy(
+                    onSuccess = { println(it) },
+                    onComplete = { println("Complete") }
+                )
+        )
+
+        quotes.onNext(mayTheOdds)
+        quotes.onNext(liveLongAndProsper)
+        quotes.onNext(mayTheForce)
+    }
+
+    exampleOf("Filter") {
+
+        val subscriptions = CompositeDisposable()
+
+        subscriptions.add(
+            Observable.fromIterable(tomatometerRatings)
+                .filter { movie ->
+                    movie.rating >= 90
+                }.subscribe {
+                    println(it)
+                }
+        )
+    }
+
+    exampleOf("SkipWhile") {
+        val subscriptions = CompositeDisposable()
+
+        subscriptions.add(
+            Observable.fromIterable(tomatometerRatings)
+                .skipWhile{ movie ->
+                    movie.rating < 90
+                }.subscribe {
+                    println(it)
+                }
+        )
+    }
+
+    exampleOf("SkipUntil"){
+        val subscriptions = CompositeDisposable()
+
+        val subject = PublishSubject.create<String>()
+        val trigger = PublishSubject.create<Unit>()
+
+        subscriptions.add(
+            subject
+                .skipUntil(trigger)
+                .subscribe {
+                    println(it)
+                }
+        )
+
+        subject.onNext(episodeI.title)
+        subject.onNext(episodeII.title)
+        subject.onNext(episodeIII.title)
+
+        trigger.onNext(Unit)
+
+        subject.onNext(episodeIV.title)
+    }
+}
